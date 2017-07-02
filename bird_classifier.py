3385ad1bbccf74104ce486010a5670b585321b43
Shubham Singh <singhcse01@gmail.com>
2017-07-02 22:43:02 +0530

    Update bird_classifier.py

--- bird_classifier.py
+++ bird_classifier.py
@@ -12,7 +12,6 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.preprocessing.image import  img_to_array
 from keras import backend as K
-# Fix for Issue - #3 https://github.com/shreyans29/thesemicolon/issues/3
 K.set_image_dim_ordering('th')
 
 import numpy as np
@@ -38,19 +37,19 @@ x=[]
 y=[]
 for fol in classes:
     print fol
-    imgfiles=os.listdir(path2+'/'+fol);
+    imgfiles=os.listdir(path2+'/'+fol)
     for img in imgfiles:
-        im=Image.open(path2+'/'+fol+'/'+img);
+        im=Image.open(path2+'/'+fol+'/'+img)
         im=im.convert(mode='RGB')
         imrs=im.resize((m,n))
-        imrs=img_to_array(imrs)/255;
-        imrs=imrs.transpose(2,0,1);
-        imrs=imrs.reshape(3,m,n);
+        imrs=img_to_array(imrs)/255
+        imrs=imrs.transpose(2,0,1)
+        imrs=imrs.reshape(3,m,n)
         x.append(imrs)
         y.append(fol)
         
-x=np.array(x);
-y=np.array(y);
+x=np.array(x)
+y=np.array(y)
 
 batch_size=32
 nb_classes=len(classes)
@@ -68,32 +67,32 @@ Y_test=np_utils.to_categorical(id_test,nb_classes)
 
 model= Sequential()
 model.add(Convolution2D(nb_filters,nb_conv,nb_conv,border_mode='same',input_shape=x_train.shape[1:]))
-model.add(Activation('relu'));
-model.add(Convolution2D(nb_filters,nb_conv,nb_conv));
-model.add(Activation('relu'));
-model.add(MaxPooling2D(pool_size=(nb_pool,nb_pool)));
-model.add(Dropout(0.30));
-model.add(Flatten());
-model.add(Dense(128));
-model.add(Dropout(0.30));
-model.add(Dense(nb_classes));
-model.add(Activation('softmax'));
+model.add(Activation('relu'))
+model.add(Convolution2D(nb_filters,nb_conv,nb_conv))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool,nb_pool)))
+model.add(Dropout(0.30))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Dropout(0.30))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
 model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
 model.summary()
-nb_epoch=10;
-batch_size=10;
+nb_epoch=10
+batch_size=10
 model.fit(x_train,Y_train,batch_size=batch_size,nb_epoch=10,verbose=1,validation_data=(x_test, Y_test))
 
 
 files=os.listdir(path1);
 img=files[2] 
-im = Image.open(path1 + '/'+img);
+im = Image.open(path1 + '/'+img)
 imrs = im.resize((m,n))
-imrs=img_to_array(imrs)/255;
-imrs=imrs.transpose(2,0,1);
-imrs=imrs.reshape(3,m,n);
+imrs=img_to_array(imrs)/255
+imrs=imrs.transpose(2,0,1)
+imrs=imrs.reshape(3,m,n)
 
 x=[]
 x.append(imrs)
-x=np.array(x);
+x=np.array(x)
 predictions = model.predict(x)
